19617e184dce38b6600ccada61b96ceac0ffb7cf
MaharaniJ <maharani.jeyaraman@outlook.com>
2023-08-10 10:49:01 +0530

    Edit-problem

--- src/EditUser.jsx
+++ src/EditUser.jsx
@@ -1,159 +1,190 @@
-import { useFormik } from 'formik'
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useFormik } from "formik";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 
 function EditUser() {
-    const params = useParams()
-    const navigate = useNavigate()
-    const formik = useFormik({
+  const params = useParams();
+  const navigate = useNavigate();
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      position: "",
+      office: "",
+      age: "",
+      startdate: "",
+      salary: "",
+    },
+    validate: (values) => {
+      let errors = {};
 
-        initialValues: {
-            name: '',
-            position: '',
-            office: '',
-            age: '',
-            startdate: '',
-            salary: ''
-        },
-        validate: (values) => {
-            let errors = {}
+      if (values.name === "") {
+        errors.name = "Please Enter name";
+      }
+      if (values.position === "") {
+        errors.position = "Please Enter Position";
+      }
 
-            if (values.name === "") {
-                errors.name = "Please Enter name"
-            }
-            if (values.position === "") {
-                errors.position = "Please Enter Position"
-            }
+      if (values.office === "") {
+        errors.office = "Please Enter Office";
+      }
 
-            if (values.office === "") {
-                errors.office = "Please Enter Office"
-            }
+      if (values.age === "") {
+        errors.age = "Please Enter Age";
+      }
 
+      if (values.startdate === "") {
+        errors.startdate = "Please Enter Startdate";
+      }
 
-            if (values.age === "") {
-                errors.age = "Please Enter Age"
-            }
-
-
-            if (values.startdate === "") {
-                errors.startdate = "Please Enter Startdate"
-            }
-
-            if (values.salary === "") {
-                errors.salary = "Please Enter Salary"
-            }
-            return errors;
-        },
-        onSubmit: async (values) => {
-            await axios.put(`https://63fcaeb9859df29986c21a62.mockapi.io/mockapi/users/${params.id}`, values)
-            navigate("/portal/users")
-
-        }
-    })
-    useEffect(() => {
-        loadUser()
-
-    }, [])
-    let loadUser = async () => {
+      if (values.salary === "") {
+        errors.salary = "Please Enter Salary";
+      }
+      return errors;
+    },
+    onSubmit: async (values) => {
         try {
-            let user = await axios.get(`https://63fcaeb9859df29986c21a62.mockapi.io/mockapi/users/${params.id}`)
-            formik.setValues({
-                name: user.data.name,
-                position: user.data.position,
-                office: user.data.office,
-                age: user.data.age,
-                startdate: user.data.startdate,
-                salary: user.data.salary
-            }
-            )
-
+          const response = await axios.put(
+            `https://645cd360e01ac61058945382.mockapi.io/users/${params.id}`,
+            values
+          );
+          
+          // Update the formik values with the updated data
+          formik.setValues(response.data);
+          
+          navigate(`/portal/users`); // Navigate to UserView with updated data
+        } catch (error) {
+          console.error(error);
         }
-        catch (error) {
+      },
+      
 
-        }
+    // onSubmit: async (values) => {
+        
+    //     await axios.put(
+    //       `https://645cd360e01ac61058945382.mockapi.io/users/${params.id}`,
+    //       values
+    //     );
+    //   navigate("/portal/users");
+    // },
+  });
+  useEffect(() => {
+    loadUser();
+  }, []);
+
+  let loadUser = async () => {
+    try {
+      let user = await axios.get(
+        `https://645cd360e01ac61058945382.mockapi.io/users/${params.id}`
+      );
+      console.log(user);
+      formik.setValues({
+        name: user.data.username,
+        position: user.data.subject,
+        office: user.data.bookname,
+        age: user.data.userid,
+        startdate: user.data.admissionon,
+        salary: user.data.bookid,
+      });
+    } catch (error) {
+        console.error(error);
     }
+  };
 
-
-    return (
-        <>
-
-            <div className='container'>
-                <form onSubmit={formik.handleSubmit}>
-                    <div className='row'>
-
-                        <div className='col-lg-6'>
-                            <label>name</label>
-                            <input
-                                className='form-control'
-                                type={"text"}
-                                value={formik.values.name}
-                                onChange={formik.handleChange}
-                                name="name">
-                            </input>
-                            <span style={{ color: 'red' }}>{formik.errors.name}</span>
-                        </div>
-                        <div className='col-lg-6'>
-                            <label>Position</label>
-                            <input className='form-control'
-                                value={formik.values.position}
-                                onChange={formik.handleChange}
-                                name="position"
-                                type={"text"}>
-                            </input>
-                            <span style={{ color: 'red' }}>{formik.errors.position}</span>
-                        </div>
-                        <div className='col-lg-6'>
-                            <label>Office</label>
-                            <input className='form-control'
-                                type={"text"}
-                                value={formik.values.office}
-                                onChange={formik.handleChange}
-                                name="office"
-                            ></input>
-                            <span style={{ color: 'red' }}>{formik.errors.office}</span>
-                        </div>
-                        <div className='col-lg-6'>
-                            <label>Age</label>
-                            <input className='form-control'
-                                value={formik.values.age}
-                                onChange={formik.handleChange}
-                                name="age"
-                                type={"text"}></input>
-                            <span style={{ color: 'red' }}>{formik.errors.age}</span>
-                        </div>
-                        <div className='col-lg-6'>
-                            <label>Start Date</label>
-                            <input className='form-control'
-                                value={formik.values.startdate}
-                                onChange={formik.handleChange}
-                                name="startdate"
-                                type={"text"}></input>
-                            <span style={{ color: 'red' }}>{formik.errors.startdate}</span>
-                        </div>
-
-                        <div className='col-lg-6'>
-                            <label>Salary</label>
-                            <input className='form-control'
-                                value={formik.values.salary}
-                                onChange={formik.handleChange}
-                                name="salary"
-                                type={"text"}></input>
-                            <span style={{ color: 'red' }}>{formik.errors.salary}</span>
-                        </div>
-
-                        <div className='col-lg-6'>
-                            <button className='btn btn-primary mt-2'
-                                type={"submit"} value={"submit"} disabled={!formik.isValid}>submit</button>
-                        </div>
-
-                    </div>
-                </form>
+  return (
+    <>
+      <div className="container">
+        <form onSubmit={formik.handleSubmit}>
+          <div className="row">
+            <div className="col-lg-6">
+              <label>name</label>
+              <input
+                className="form-control"
+                type={"text"}
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                name="name"
+              ></input>
+              <span style={{ color: "red" }}>{formik.errors.name}</span>
             </div>
-        </>
+            <div className="col-lg-6">
+              <label>Position</label>
+              <input
+                className="form-control"
+                value={formik.values.position}
+                onChange={formik.handleChange}
+                name="position"
+                type={"text"}
+              ></input>
+              <span style={{ color: "red" }}>{formik.errors.position}</span>
+            </div>
+            <div className="col-lg-6">
+              <label>Office</label>
+              <input
+                className="form-control"
+                type={"text"}
+                value={formik.values.office}
+                onChange={formik.handleChange}
+                name="office"
+              ></input>
+              <span style={{ color: "red" }}>{formik.errors.office}</span>
+            </div>
+            <div className="col-lg-6">
+              <label>Age</label>
+              <input
+                className="form-control"
+                value={formik.values.age}
+                onChange={formik.handleChange}
+                name="age"
+                type={"text"}
+              ></input>
+              <span style={{ color: "red" }}>{formik.errors.age}</span>
+            </div>
+            <div className="col-lg-6">
+              <label>Start Date</label>
+              <input
+                className="form-control"
+                value={formik.values.startdate}
+                onChange={formik.handleChange}
+                name="startdate"
+                type={"text"}
+              ></input>
+              <span style={{ color: "red" }}>{formik.errors.startdate}</span>
+            </div>
 
+            <div className="col-lg-6">
+              <label>Salary</label>
+              <input
+                className="form-control"
+                value={formik.values.salary}
+                onChange={formik.handleChange}
+                name="salary"
+                type={"text"}
+              ></input>
+              <span style={{ color: "red" }}>{formik.errors.salary}</span>
+            </div>
 
-    )
+            <div className="col-lg-6">
+              <button
+                className="btn btn-primary mt-2"
+                type={"submit"}
+                value={"submit"}
+                disabled={!formik.isValid}
+              >
+                submit
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default EditUser
+export default EditUser;
+
+
+
+
+
+
--- src/Users.jsx
+++ src/Users.jsx
@@ -1,112 +1,131 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Users() {
-    const [users, setUsers] = useState([])
-    const [isLoading, setLoading] = useState(false)
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(false);
 
-    useEffect(() => {
-        loaddata()
+  useEffect(() => {
+    loaddata();
+  }, []);
 
-    }, [])
+  let loaddata = async () => {
+    setLoading(true);
+    let users = await axios.get(
+      `https://645cd360e01ac61058945382.mockapi.io/users`
+    );
+    console.log(users);
+    setUsers(users.data);
+    setLoading(false);
+  };
+  let userDelete = async (id) => {
+    try {
+      let ask = window.confirm("Do you want to delete?");
+      if (ask) {
+        await axios.delete(
+          `https://645cd360e01ac61058945382.mockapi.io/users/${id}`
+        );
+        loaddata();
+      }
+    } catch (error) {}
+  };
 
-    let loaddata = async () => {
-        setLoading(true)
-        let users = await axios.get(`https://63fcaeb9859df29986c21a62.mockapi.io/mockapi/users`)
-        setUsers(users.data)
-        setLoading(false)
-    }
-    let userDelete = async (id) => {
-        try {
-            let ask = window.confirm("Do you want to delete?")
-            if (ask) {
-                await axios.delete(`https://63fcaeb9859df29986c21a62.mockapi.io/mockapi/users/${id}`)
-                loaddata()
-            }
-
-        }
-        catch (error) {
-
-        }
-    }
-
-    return (
-        <div className="container-fluid">
-            {/* <!-- Page Heading --> */}
-            <div className='d-sm-flex align-items-center justify-content-between mb-4'>
-                <h1 className="h3 mb-0 text-gray-800">Users</h1>
-                <Link to="/portal/createuser" className={"d-none d-sm-inline-block btn-sm btn-primary shadow-sm"}>
-                    <li className='fas fa-download fa-sm text-white-50'></li>Create User</Link>
+  return (
+    <div className="container-fluid">
+      {/* <!-- Page Heading --> */}
+      <div className="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 className="h3 mb-0 text-gray-800">Users</h1>
+        <Link
+          to="/portal/createuser"
+          className={"d-none d-sm-inline-block btn-sm btn-primary shadow-sm"}
+        >
+          <li className="fas fa-download fa-sm text-white-50"></li>Create User
+        </Link>
+      </div>
+      {/* <!-- DataTales --> */}
+      {isLoading ? (
+        <span>Loading...</span>
+      ) : (
+        <div className="card shadow mb-4">
+          <div className="card-header py-3">
+            <h6 className="m-0 font-weight-bold text-primary">DataTables</h6>
+          </div>
+          <div className="card-body">
+            <div className="table-responsive">
+              <table
+                className="table table-bordered"
+                id="dataTable"
+                width="100%"
+                cellSpacing="0"
+              >
+                <thead>
+                  <tr>
+                    <th>#Sl</th>
+                    <th>Name</th>
+                    <th>Position</th>
+                    <th>Office</th>
+                    <th>Age</th>
+                    <th>Start date</th>
+                    <th>Salary</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tfoot>
+                  <tr>
+                    <th>#Sl</th>
+                    <th>Action</th>
+                    <th>Name</th>
+                    <th>Position</th>
+                    <th>Office</th>
+                    <th>Age</th>
+                    <th>Start date</th>
+                    <th>Salary</th>
+                  </tr>
+                </tfoot>
+                <tbody>
+                  {users.map((user, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{user.username}</td>
+                        <td>{user.subject}</td>
+                        <td>{user.bookname}</td>
+                        <td>{user.userid}</td>
+                        <td>{user.admissionon}</td>
+                        <td>{user.bookid}</td>
+                        <td>
+                          <Link
+                            to={`/portal/users/${user.id}`}
+                            className="btn btn-sm btn-warning mr-2"
+                          >
+                            View
+                          </Link>
+                          <Link
+                            to={`/portal/users/edit/${user.id}`}
+                            className="btn btn-sm btn-primary mr-2"
+                          >
+                            Edit
+                          </Link>
+                          <button
+                            onClick={() => userDelete(user.id)}
+                            className="btn btn-sm btn-danger mr-2"
+                          >
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
             </div>
-            {/* <!-- DataTales --> */}
-            {
-                isLoading ? <span>Loading...</span> :
-                    <div className="card shadow mb-4">
-                        <div className="card-header py-3">
-                            <h6 className="m-0 font-weight-bold text-primary">DataTables</h6>
-                        </div>
-                        <div className="card-body">
-                            <div className="table-responsive">
-                                <table className="table table-bordered" id="dataTable" width="100%" cellSpacing="0">
-                                    <thead>
-                                        <tr>
-                                            <th>#Sl</th>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tfoot>
-                                        <tr>
-                                            <th>#Sl</th>
-                                            <th>Action</th>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
-                                        </tr>
-                                    </tfoot>
-                                    <tbody>
-                                        {
-                                            users.map((user, index) => {
-                                                return (
-                                                    <tr key={index}>
-                                                        <td>{index + 1}</td>
-                                                        <td>{user.name}</td>
-                                                        <td>{user.position}</td>
-                                                        <td>{user.office}</td>
-                                                        <td>{user.age}</td>
-                                                        <td>{user.startdate}</td>
-                                                        <td>${user.salary}</td>
-                                                        <td> <Link to={`/portal/users/${user.id}`} className='btn btn-sm btn-warning mr-2'>View</Link>
-                                                            <Link to={`/portal/users/edit/${user.id}`} className='btn btn-sm btn-primary mr-2'>Edit</Link>
-                                                            <button onClick={() => userDelete(user.id)} className='btn btn-sm btn-danger mr-2'>Delete</button>
-                                                        </td>
-                                                    </tr>
-                                                )
-                                            })
-                                        }
-
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-
-            }
+          </div>
         </div>
-        // <!-- /.container-fluid -->
-
-
-    )
+      )}
+    </div>
+    // <!-- /.container-fluid -->
+  );
 }
 
 export default Users;
